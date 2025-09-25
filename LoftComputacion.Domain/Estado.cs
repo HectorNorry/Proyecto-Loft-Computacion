e70c38bbfe9f37d7f3310a716ab3923ece0cc929
Hector Norry <piscudelag@gmail.com>
2025-09-25 00:15:02 -0300

    feat(domain): Agrega modelos detallados y actualiza la orden

--- LoftComputacion.Domain/Estado.cs
+++ LoftComputacion.Domain/Estado.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace LoftComputacion.Domain
 {
-    internal class Estado
+    public class Estado
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
--- LoftComputacion.Domain/MetodoDePago.cs
+++ LoftComputacion.Domain/MetodoDePago.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace LoftComputacion.Domain
 {
-    internal class MetodoDePago
+    public class MetodoDePago
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
