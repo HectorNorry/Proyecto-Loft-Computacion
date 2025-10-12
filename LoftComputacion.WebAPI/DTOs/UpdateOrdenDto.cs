cae398f5919862ab63af8be5ecfd4cad08cf8441
Hector Norry <piscudelag@gmail.com>
2025-10-12 09:39:53 -0300

    feat (api): Se implementa endpoint PUT para OrdendesDeServicio

---
+++ LoftComputacion.WebAPI/DTOs/UpdateOrdenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LoftComputacion.WebAPI.DTOs
+{
+    public class UpdateOrdenDto
+    {
+        public int EstadoId { get; set; }
+        public decimal? PrecioPresupuestado { get; set; }
+        public decimal? PrecioFinal { get; set; }
+    }
+}
